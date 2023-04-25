94f24823960709b320a4d76e51807d8810c6441f
Tylor Yang <tylor.yang@gmail.com>
2023-04-25 15:54:29 +0800

    Add LFS options to Makefile

--- Application.mk
+++ Application.mk
@@ -2,6 +2,6 @@ APP_PROJECT_PATH := $(NDK_PROJECT_PATH)
 
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/hx_util_src/Android.mk
 
-APP_ABI := arm64-v8a x86_64
+APP_ABI := arm64-v8a x86_64 armeabi-v7a
 APP_OPTIM := release
 APP_PLATFORM := android-21
